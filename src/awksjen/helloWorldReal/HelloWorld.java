b05828f27abc5c656324dba6ae7968ffe9d9dc53
AwksJen <awksjen@gmail.com>
2020-04-27 10:37:55 -0500

    fisrst java helloWorld

---
+++ src/awksjen/helloWorldReal/HelloWorld.java
@@ -0,0 +1,18 @@
+package awksjen.helloWorldReal;
+
+import com.godtsoft.diyjava.DIYWindow;
+
+public class HelloWorld extends DIYWindow {
+
+	public HelloWorld() {
+		print("Hello, eathling.");
+		print("Take me to your leader.");
+		
+	}
+
+	public static void main(String[] args) {
+		new HelloWorld();
+		
+	}
+
+}
